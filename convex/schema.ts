d7142ec56557ef7157200fa1ad3c689bcd02242a
Jan Balin <jgbalin@gmail.com>
2024-05-01 11:54:02 +0200

    chore: setup database schema

---
+++ convex/schema.ts
@@ -0,0 +1,11 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+// Leveraging the auto-generated _id field of Convex
+export default defineSchema({
+  notes: defineTable({
+    title: v.string(),
+    content: v.string(),
+    _id: v.string(),
+  }),
+});
